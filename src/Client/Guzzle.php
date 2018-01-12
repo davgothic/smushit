08f08dca8085b677049cdbd819be82f5bebc3495
Dav <davgothic@gmail.com>
2018-01-12 21:56:05 +0000

    Add client for Guzzle PHP HTTP client

---
+++ src/Client/Guzzle.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace DavGothic\SmushIt\Client;
+
+use DavGothic\SmushIt\SmushIt;
+use GuzzleHttp\Client as GuzzleClient;
+
+/**
+ * A Guzzle SmushIt client
+ *
+ * @codeCoverageIgnore
+ */
+class Guzzle extends Client
+{
+
+	/**
+	 * @var GuzzleClient|null
+	 */
+	private $guzzleClient;
+
+	/**
+	 * Guzzle constructor.
+	 *
+	 * @param GuzzleClient|null $guzzleClient A GuzzleClient instance.
+	 */
+	public function __construct(GuzzleClient $guzzleClient = null)
+	{
+		if (null === $guzzleClient) {
+			$this->guzzleClient = new GuzzleClient(array('timeout' => $this->timeout));
+		} else {
+			$this->guzzleClient = $guzzleClient;
+		}
+	}
+
+    /**
+     * @inheritdoc
+     */
+    public function execute($requestType, $imageLocation)
+    {
+        if (self::TYPE_LOCAL === $requestType) {
+            $res = $this->guzzleClient->post(SmushIt::SMUSH_URL, array(
+                'multipart' => array(
+                    array(
+                        'name'     => 'files',
+                        'contents' => fopen($imageLocation, 'rb'),
+                    )
+                )
+            ));
+        } else {
+            $res = $this->guzzleClient->get(SmushIt::SMUSH_URL, array(
+                'query' => array(
+                    'img' => $imageLocation,
+                ),
+            ));
+        }
+
+        return $res->getBody();
+    }
+
+}
